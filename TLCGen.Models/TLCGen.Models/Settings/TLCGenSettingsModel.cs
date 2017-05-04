90aa4a6ff862168da282ad63fdeb5b71642aa441
menno doeterniettoe <mennowo@gmail.com>
2017-05-04 15:12:56 +0200

    Remove wrong reference from Models project

--- TLCGen.Models/TLCGen.Models/Settings/TLCGenSettingsModel.cs
+++ TLCGen.Models/TLCGen.Models/Settings/TLCGenSettingsModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace TLCGen.Models.Settings
 {
