f624e9472468027df3b4dd6f7d093ee041e73a1e
menno doeterniettoe <mennowo@gmail.com>
2017-08-03 17:45:21 +0200

    Update to version

--- TLCGen/Properties/AssemblyInfo.cs
+++ TLCGen/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("TLCGen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("CodingConnected e.U.")]
 [assembly: AssemblyProduct("TLCGen")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.47.0")]
-[assembly: AssemblyFileVersion("0.1.47.0")]
+[assembly: AssemblyVersion("0.1.81.0")]
+[assembly: AssemblyFileVersion("0.1.81.0")]
