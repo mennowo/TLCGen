1b05ad8a51d14d2e415a40eab8fed58c8f31cdce
menno <menno@DESKTOP-6SF2R34>
2018-10-03 09:51:11 +0200

    Created abstraction for adding/removing items logic

---
+++ TLCGen.Dependencies/TLCGen.Helpers/AddRemoveItemsManager.cs
@@ -0,0 +1,140 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TLCGen.Extensions;
+using RelayCommandWpf = GalaSoft.MvvmLight.CommandWpf.RelayCommand;
+
+namespace TLCGen.Helpers
+{
+    public class AddRemoveItemsManager<T1, T2, T3> : ViewModelBase where T1 : IViewModelWithItem
+    {
+        public ObservableCollectionAroundList<T1, T2> ItemsSource { get; }
+        public ObservableCollection<T3> AllSelectableItems { get; } = new ObservableCollection<T3>();
+        public ObservableCollection<T3> SelectableItems { get; } = new ObservableCollection<T3>();
+        
+        private T3 _selectedItemToAdd;
+        public T3 SelectedItemToAdd
+        {
+            get => _selectedItemToAdd;
+            set
+            {
+                _selectedItemToAdd = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private T1 _selectedItem;
+        public T1 SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                _selectedItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Func<T3, T1> GetNewItem { get; }
+        public Func<T1, T3, bool> SelectableEqualsItem { get; }
+
+        RelayCommandWpf _addItemCommand;
+        public ICommand AddItemCommand => _addItemCommand ?? (_addItemCommand = new RelayCommandWpf(AddItemCommand_executed, AddItemCommand_canExecute));
+
+        private bool AddItemCommand_canExecute()
+        {
+            return SelectedItemToAdd != null;
+        }
+
+        private void AddItemCommand_executed()
+        {
+            var i = SelectableItems.IndexOf(SelectedItemToAdd);
+            ItemsSource.Add(GetNewItem(SelectedItemToAdd));
+            ItemsSource.BubbleSort();
+            UpdateSelectables(null);
+            if (SelectableItems.Any())
+            {
+                if (i >= 0 && SelectableItems.Count > i) SelectedItemToAdd = SelectableItems[i];
+                else SelectedItemToAdd = SelectableItems.Last();
+            }
+            else
+            {
+                SelectedItemToAdd = default(T3);
+            }
+        }
+
+        RelayCommandWpf _removeItemCommand;
+        public ICommand RemoveItemCommand => _removeItemCommand ?? (_removeItemCommand = new RelayCommandWpf(RemoveItemCommand_executed, RemoveItemCommand_canExecute));
+
+        private bool RemoveItemCommand_canExecute()
+        {
+            return SelectedItem != null;
+        }
+
+        private void RemoveItemCommand_executed()
+        {
+            var iSelectedItem = ItemsSource.IndexOf(SelectedItem);
+            var removedSelToAddItem = AllSelectableItems.FirstOrDefault(x => SelectableEqualsItem(SelectedItem, x));
+            var iSelectedToAdd = SelectableItems.IndexOf(SelectedItemToAdd);
+            ItemsSource.Remove(SelectedItem);
+            if (ItemsSource.Count > 0)
+            {
+                if (iSelectedItem >= 0 && ItemsSource.Count > iSelectedItem) SelectedItem = ItemsSource[iSelectedItem];
+                else SelectedItem = ItemsSource.Last();
+            }
+            else
+            {
+                SelectedItem = default(T1);
+            }
+            UpdateSelectables(null);
+            if (SelectableItems.Any())
+            {
+                if(removedSelToAddItem != null && SelectableItems.Any(x => x.Equals(removedSelToAddItem)))
+                {
+                    SelectedItemToAdd = removedSelToAddItem;
+                }
+                else
+                {
+                    if (iSelectedToAdd >= 0 && iSelectedToAdd < SelectableItems.Count) SelectedItemToAdd = SelectableItems[iSelectedToAdd];
+                    else SelectedItemToAdd = SelectableItems.Last();
+                }
+            }
+            else
+            {
+                SelectedItemToAdd = default(T3);
+            }
+        }
+
+        public void UpdateSelectables(IEnumerable<T3> allItems)
+        {
+            if (allItems != null)
+            {
+                AllSelectableItems.Clear();
+                foreach (var i in allItems)
+                {
+                    AllSelectableItems.Add(i);
+                }
+            }
+            SelectableItems.Clear();
+            foreach (var i in AllSelectableItems)
+            {
+                if (!ItemsSource.Any(x => SelectableEqualsItem(x, i)))
+                {
+                    SelectableItems.Add(i);
+                }
+            }
+            if (SelectableItems.Any()) SelectedItemToAdd = SelectableItems[0];
+        }
+
+        public AddRemoveItemsManager(ObservableCollectionAroundList<T1, T2> itemsSource, Func<T3, T1> getNewItem, Func<T1, T3, bool> selectableEqualsItem)
+        {
+            ItemsSource = itemsSource;
+            GetNewItem = getNewItem;
+            SelectableEqualsItem = selectableEqualsItem;
+        }
+    }
+}
--- TLCGen.PluggedInItems/TLCGen.Plugins.AFM/AFMTabViewModel.cs
+++ TLCGen.PluggedInItems/TLCGen.Plugins.AFM/AFMTabViewModel.cs
@@ -24,20 +24,8 @@ namespace TLCGen.Plugins.AFM
         #region Properties
 
         public List<string> AllFasen { get; } = new List<string>();
-        public ObservableCollection<string> SelectableFasen { get; } = new ObservableCollection<string>();
         private ObservableCollection<string> _SelectableDummyFasen = new ObservableCollection<string>();
         public ObservableCollection<string> SelectableDummyFasen => _SelectableDummyFasen;
-
-        private string _selectedFaseToAdd;
-        public string SelectedFaseToAdd
-        {
-            get => _selectedFaseToAdd;
-            set
-            {
-                _selectedFaseToAdd = value;
-                RaisePropertyChanged();
-            }
-        }
 
         private AFMFaseCyclusDataViewModel _selectedAFMFase;
         public AFMFaseCyclusDataViewModel SelectedAFMFase
@@ -61,8 +49,6 @@ namespace TLCGen.Plugins.AFM
             }
         }
 
-        public ObservableCollectionAroundList<AFMFaseCyclusDataViewModel, AFMFaseCyclusDataModel> AFMFasen { get; private set; }
-
         public bool AFMToepassen
         {
             get => _afmModel.AFMToepassen;
@@ -73,76 +59,25 @@ namespace TLCGen.Plugins.AFM
             }
         }
 
+        public ObservableCollectionAroundList<AFMFaseCyclusDataViewModel, AFMFaseCyclusDataModel> AFMFasen { get; private set; }
+
+        private AddRemoveItemsManager<AFMFaseCyclusDataViewModel, AFMFaseCyclusDataModel, string> _AFMFasenManager;
+        public AddRemoveItemsManager<AFMFaseCyclusDataViewModel, AFMFaseCyclusDataModel, string> AFMFasenManager =>
+            _AFMFasenManager ?? (_AFMFasenManager = new AddRemoveItemsManager<AFMFaseCyclusDataViewModel, AFMFaseCyclusDataModel, string>(
+                AFMFasen,
+                (x => new AFMFaseCyclusDataViewModel(new AFMFaseCyclusDataModel
+                {
+                    FaseCyclus = x,
+                    DummyFaseCyclus = "NG",
+                    MinimaleGroentijd = 6,
+                    MaximaleGroentijd = 80
+                })),
+                ((x1, x2) => x1.FaseCyclus == x2)
+                ));
+
         #endregion // Properties
 
         #region Commands
-
-        RelayCommand _addFaseCommand;
-        public ICommand AddFaseCommand => _addFaseCommand ?? (_addFaseCommand = new RelayCommand(AddFaseCommand_executed, AddFaseCommand_canExecute));
-
-        private bool AddFaseCommand_canExecute()
-        {
-            return !string.IsNullOrWhiteSpace(SelectedFaseToAdd);
-        }
-
-        private void AddFaseCommand_executed()
-        {
-            var i = SelectableFasen.IndexOf(SelectedFaseToAdd);
-            AFMFasen.Add(new AFMFaseCyclusDataViewModel(new AFMFaseCyclusDataModel
-            {
-                FaseCyclus = SelectedFaseToAdd,
-                DummyFaseCyclus = "NG",
-                MinimaleGroentijd = 6,
-                MaximaleGroentijd = 80
-            }));
-            UpdateSelectableFasen(null);
-            if (SelectableFasen.Any())
-            {
-                if (i >= 0 && SelectableFasen.Count > i) SelectedFaseToAdd = SelectableFasen[i];
-                else SelectedFaseToAdd = SelectableFasen.Last();
-            }
-            else
-            {
-                SelectedFaseToAdd = null;
-            }
-            AFMFasen.BubbleSort();
-            MessengerInstance.Send(new ControllerDataChangedMessage());
-        }
-
-        RelayCommand _removeFaseCommand;
-        public ICommand RemoveFaseCommand => _removeFaseCommand ?? (_removeFaseCommand = new RelayCommand(RemoveFaseCommand_executed, RemoveFaseCommand_canExecute));
-
-        private bool RemoveFaseCommand_canExecute()
-        {
-            return SelectedAFMFase != null;
-        }
-
-        private void RemoveFaseCommand_executed()
-        {
-            var i = AFMFasen.IndexOf(SelectedAFMFase);
-            var iA = SelectableFasen.IndexOf(SelectedFaseToAdd);
-            AFMFasen.Remove(SelectedAFMFase);
-            if (AFMFasen.Count > 0)
-            {
-                if (i >= 0 && AFMFasen.Count > i) SelectedAFMFase = AFMFasen[i];
-                else SelectedAFMFase = AFMFasen.Last();
-            }
-            else
-            {
-                SelectedAFMFase = null;
-            }
-            UpdateSelectableFasen(null);
-            if (SelectableFasen.Any())
-            {
-                if (iA >= 0 && SelectableFasen.Count > iA) SelectedFaseToAdd = SelectableFasen[iA];
-                else SelectedFaseToAdd = SelectableFasen.Last();
-            }
-            else
-            {
-                SelectedFaseToAdd = null;
-            }
-            MessengerInstance.Send(new ControllerDataChangedMessage());
-        }
 
         #endregion // Commands
 
@@ -192,24 +127,25 @@ namespace TLCGen.Plugins.AFM
         {
             if(allFasen != null)
             {
-                AllFasen.Clear();
+            //    AllFasen.Clear();
                 _SelectableDummyFasen.Clear();
+                _SelectableDummyFasen.Add("NG");
                 foreach (var f in allFasen)
                 {
-                    AllFasen.Add(f);
+            //        AllFasen.Add(f);
                     _SelectableDummyFasen.Add(f);
                 }
-                _SelectableDummyFasen.Add("NG");
             }
-            SelectableFasen.Clear();
-            foreach(var f in AllFasen)
-            {
-                if(!AFMFasen.Any(x => x.FaseCyclus == f))
-                {
-                    SelectableFasen.Add(f);
-                }
-            }
-            if(SelectableFasen.Any()) SelectedFaseToAdd = SelectableFasen[0];
+            //SelectableFasen.Clear();
+            //foreach(var f in AllFasen)
+            //{
+            //    if(!AFMFasen.Any(x => x.FaseCyclus == f))
+            //    {
+            //        SelectableFasen.Add(f);
+            //    }
+            //}
+            //if(SelectableFasen.Any()) SelectedFaseToAdd = SelectableFasen[0];
+            AFMFasenManager.UpdateSelectables(allFasen);
         }
 
         #endregion // Public Methods
