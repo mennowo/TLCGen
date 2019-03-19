f59a40d02d3f14e5621623351531d48ce1e24c1a
Menno van der Woude <mennowo@gmail.com>
2019-03-19 10:42:06 +0100

    Alternatieve opmaak tabc voor signaalgroepinstellingen

--- TLCGen.PluggedInItems/TLCGen.Generators.CCOL/CodeGeneration/Sources/CCOLGeneratorGenerateTabC.cs
+++ TLCGen.PluggedInItems/TLCGen.Generators.CCOL/CodeGeneration/Sources/CCOLGeneratorGenerateTabC.cs
@@ -201,16 +201,23 @@ namespace TLCGen.Generators.CCOL.CodeGeneration
 
             foreach (FaseCyclusModel fcm in controller.Fasen)
             {
-                string s = $"   FC_code[{fcm.GetDefine()}] = \"{fcm.Naam}\"; TRG_max[{fcm.GetDefine()}] = {fcm.TRG};";
-                int i = s.Length;
+                string s = $"   FC_code[{fcm.GetDefine()}] = \"{fcm.Naam}\"; TRG_max[{fcm.GetDefine()}] = {fcm.TRG}; ";
+                //int i = s.Length;
                 sb.AppendLine(s);
-                sb.AppendLine($"TRG_min[{fcm.GetDefine()}] = {fcm.TRG_min};".PadLeft(i));
-                sb.AppendLine($"TGG_max[{fcm.GetDefine()}] = {fcm.TGG};".PadLeft(i));
-                sb.AppendLine($"TGG_min[{fcm.GetDefine()}] = {fcm.TGG_min};".PadLeft(i));
-                sb.AppendLine($"TFG_max[{fcm.GetDefine()}] = {fcm.TFG};".PadLeft(i));
-                sb.AppendLine($"TGL_max[{fcm.GetDefine()}] = {fcm.TGL};".PadLeft(i));
-                sb.AppendLine($"TGL_min[{fcm.GetDefine()}] = {fcm.TGL_min};".PadLeft(i));
-                sb.AppendLine($"TVG_max[{fcm.GetDefine()}] = NG;".PadLeft(i));
+                //sb.AppendLine($"TRG_min[{fcm.GetDefine()}] = {fcm.TRG_min};".PadLeft(i));
+                //sb.AppendLine($"TGG_max[{fcm.GetDefine()}] = {fcm.TGG};".PadLeft(i));
+                //sb.AppendLine($"TGG_min[{fcm.GetDefine()}] = {fcm.TGG_min};".PadLeft(i));
+                //sb.AppendLine($"TFG_max[{fcm.GetDefine()}] = {fcm.TFG};".PadLeft(i));
+                //sb.AppendLine($"TGL_max[{fcm.GetDefine()}] = {fcm.TGL};".PadLeft(i));
+                //sb.AppendLine($"TGL_min[{fcm.GetDefine()}] = {fcm.TGL_min};".PadLeft(i));
+                //sb.AppendLine($"TVG_max[{fcm.GetDefine()}] = NG;".PadLeft(i));
+                sb.Append($"TRG_min[{fcm.GetDefine()}] = {fcm.TRG_min}; ");
+                sb.Append($"TGG_max[{fcm.GetDefine()}] = {fcm.TGG}; ");
+                sb.Append($"TGG_min[{fcm.GetDefine()}] = {fcm.TGG_min}; ");
+                sb.Append($"TFG_max[{fcm.GetDefine()}] = {fcm.TFG}; ");
+                sb.Append($"TGL_max[{fcm.GetDefine()}] = {fcm.TGL}; ");
+                sb.Append($"TGL_min[{fcm.GetDefine()}] = {fcm.TGL_min}; ");
+                sb.AppendLine($"TVG_max[{fcm.GetDefine()}] = NG;");
             }
 
             return sb.ToString();
