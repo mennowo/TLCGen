510c3afcd741c9f1e918833651a310ab594850ff
Menno van der Woude <mennowo@gmail.com>
2020-05-26 08:53:01 +0200

    Version 0.7.9.0

--- TLCGen.UITests/UnitTest1.cs
+++ TLCGen.UITests/UnitTest1.cs
@@ -8,34 +8,6 @@ using OpenQA.Selenium.Appium.Windows;
 
 namespace TLCGen.UITests
 {
-    public static class Utility
-    {
-        public static WindowsElement FindElementByAccessibilityIdRetry(this WindowsDriver<WindowsElement> desktopSession, string xPath, int nTryCount = 15)
-        {
-            WindowsElement uiTarget = null;
-
-            while (nTryCount-- > 0)
-            {
-                try
-                {
-                    uiTarget = desktopSession.FindElementByTagName(xPath);
-                }
-                catch {}
-
-                if (uiTarget != null)
-                {
-                    break;
-                }
-                else
-                {
-                    System.Threading.Thread.Sleep(2000);
-                }
-            }
-
-            return uiTarget;
-        }
-    }
-
     [TestFixture]
     public class UnitTest1
     {
@@ -64,41 +36,106 @@ namespace TLCGen.UITests
         }
 
         [Test]
-        public void Testing()
+        public void StartTLCGenWithNewController_Add4SignalGroups_CorrectNumberAdded()
         {
             var app = FlaUI.Core.Application.Launch(@"C:\Users\menno\CodingConnected\Repos\TLCGen\TLCGen\bin\Debug\TLCGen.exe");
             using (var automation = new UIA3Automation())
             {
+                // start
                 var window = app.GetMainWindow(automation);
-                var unkownMenumainMenu = window.FindFirstDescendant(e => e.ByAutomationId("mainMenu"));
-                var fileMenuItemfileMenuItem = unkownMenumainMenu.FindFirstDescendant(e => e.ByAutomationId("fileMenuItem"));
+                var windowMainMenu = window.FindFirstDescendant(e => e.ByAutomationId("mainMenu"));
+
+                // new file
+                var fileMenuItemfileMenuItem = windowMainMenu.FindFirstDescendant(e => e.ByAutomationId("fileMenuItem"));
                 fileMenuItemfileMenuItem.Click();
-                // Wait a bit for the animation 
                 System.Threading.Thread.Sleep(500);
                 var newMenuItemnewFileMenuItem = fileMenuItemfileMenuItem.FindFirstDescendant(e => e.ByAutomationId("newFileMenuItem"));
                 newMenuItemnewFileMenuItem.Click();
-                // Wait a bit for the animation 
                 System.Threading.Thread.Sleep(500);
-                var unkownCustomcontrollerView = window.FindFirstDescendant(e => e.ByAutomationId("controllerView"));
-                var unkownTabcontrollerViewMainTab = unkownCustomcontrollerView.FindFirstDescendant(e => e.ByAutomationId("controllerViewMainTab"));
-                var tLCGenViewModelsFasenTabViewModelTabItem_1 = unkownTabcontrollerViewMainTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenTabViewModel"));
-                tLCGenViewModelsFasenTabViewModelTabItem_1.Click();
-                var unkownCustom_1 = tLCGenViewModelsFasenTabViewModelTabItem_1.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
-                var unkownTabfasenTab = unkownCustom_1.FindFirstDescendant(e => e.ByAutomationId("fasenTab"));
-                var tLCGenViewModelsFasenLijstTabViewModelTabItem_1 = unkownTabfasenTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenLijstTabViewModel"));
-                var unkownCustomfasenLijstTab = tLCGenViewModelsFasenLijstTabViewModelTabItem_1.FindFirstDescendant(e => e.ByAutomationId("fasenLijstTab"));
-                var unkownCustom_2 = unkownCustomfasenLijstTab.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
-                var fasetoevButton_1 = unkownCustom_2.FindFirstChild(e => e.ByName("Fase toev."));
-                fasetoevButton_1.Click();
-                fasetoevButton_1.Click();
-                fasetoevButton_1.Click();
-                fasetoevButton_1.Click();
+
+                // controller view & tabs
+                var controllerView = window.FindFirstDescendant(e => e.ByAutomationId("controllerView"));
+                var controllerViewMainTab = controllerView.FindFirstDescendant(e => e.ByAutomationId("controllerViewMainTab"));
+                var controllerViewMainTabFasenButton = controllerViewMainTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenTabViewModel"));
+                controllerViewMainTabFasenButton.Click();
+                var fasenTabContentTemplate = controllerViewMainTabFasenButton.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
+                var fasenTab = fasenTabContentTemplate.FindFirstDescendant(e => e.ByAutomationId("fasenTab"));
+                var fasenTabLijstTabButton = fasenTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenLijstTabViewModel"));
+                var fasenTabLijstTab = fasenTabLijstTabButton.FindFirstDescendant(e => e.ByAutomationId("fasenLijstTab"));
+                var fasenTabLijstTabAddRemoveControl = fasenTabLijstTab.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
+                var fasenTabLijstTabAddRemoveControlAddButton = fasenTabLijstTabAddRemoveControl.FindFirstChild(e => e.ByName("Fase toev."));
+
+                // action!
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                
+                // assert
+                var fasenLijstDataGrid = fasenTabLijstTab.FindFirstChild(e => e.ByAutomationId("fasenLijstDataGrid")).AsDataGridView();
+                Assert.AreEqual(fasenLijstDataGrid.Rows.Length, 4);
+                
+                // close
                 fileMenuItemfileMenuItem.Click();
-                // Wait a bit for the animation 
                 System.Threading.Thread.Sleep(500);
                 var exitMenuItemexitMenuItem = fileMenuItemfileMenuItem.FindFirstDescendant(e => e.ByAutomationId("exitMenuItem"));
                 exitMenuItemexitMenuItem.Click();
-                // Wait a bit for the animation 
+                System.Threading.Thread.Sleep(500);
+                var deregelingisgewijzigdOpslaanWindow_1 = window.FindFirstChild(e => e.ByName("De regeling is gewijzigd. Opslaan?"));
+                var noButton7 = deregelingisgewijzigdOpslaanWindow_1.FindFirstDescendant(e => e.ByAutomationId("7"));
+                noButton7.Click();
+            }
+        }
+
+        
+        [Test]
+        public void StartTLCGenWithNewController_Add4SignalGroupsThenRemove1_CorrectNumberRemains()
+        {
+            var app = FlaUI.Core.Application.Launch(@"C:\Users\menno\CodingConnected\Repos\TLCGen\TLCGen\bin\Debug\TLCGen.exe");
+            using (var automation = new UIA3Automation())
+            {
+                // start
+                var window = app.GetMainWindow(automation);
+                var windowMainMenu = window.FindFirstDescendant(e => e.ByAutomationId("mainMenu"));
+
+                // new file
+                var fileMenuItemfileMenuItem = windowMainMenu.FindFirstDescendant(e => e.ByAutomationId("fileMenuItem"));
+                fileMenuItemfileMenuItem.Click();
+                System.Threading.Thread.Sleep(500);
+                var newMenuItemnewFileMenuItem = fileMenuItemfileMenuItem.FindFirstDescendant(e => e.ByAutomationId("newFileMenuItem"));
+                newMenuItemnewFileMenuItem.Click();
+                System.Threading.Thread.Sleep(500);
+
+                // controller view & tabs
+                var controllerView = window.FindFirstDescendant(e => e.ByAutomationId("controllerView"));
+                var controllerViewMainTab = controllerView.FindFirstDescendant(e => e.ByAutomationId("controllerViewMainTab"));
+                var controllerViewMainTabFasenButton = controllerViewMainTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenTabViewModel"));
+                controllerViewMainTabFasenButton.Click();
+                var fasenTabContentTemplate = controllerViewMainTabFasenButton.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
+                var fasenTab = fasenTabContentTemplate.FindFirstDescendant(e => e.ByAutomationId("fasenTab"));
+                var fasenTabLijstTabButton = fasenTab.FindFirstChild(e => e.ByName("TLCGen.ViewModels.FasenLijstTabViewModel"));
+                var fasenTabLijstTab = fasenTabLijstTabButton.FindFirstDescendant(e => e.ByAutomationId("fasenLijstTab"));
+                var fasenTabLijstTabAddRemoveControl = fasenTabLijstTab.FindFirstChild(e => e.ByControlType(FlaUI.Core.Definitions.ControlType.Custom));
+                var fasenTabLijstTabAddRemoveControlAddButton = fasenTabLijstTabAddRemoveControl.FindFirstChild(e => e.ByName("Fase toev."));
+                var fasenTabLijstTabAddRemoveControlRemoveButton = fasenTabLijstTabAddRemoveControl.FindFirstChild(e => e.ByName("Fase verw."));
+                var fasenLijstDataGrid = fasenTabLijstTab.FindFirstChild(e => e.ByAutomationId("fasenLijstDataGrid")).AsDataGridView();
+
+                // action!
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenTabLijstTabAddRemoveControlAddButton.Click();
+                fasenLijstDataGrid.Rows[0].Click();
+                fasenTabLijstTabAddRemoveControlRemoveButton.Click();
+                
+                // assert
+                Assert.AreEqual(fasenLijstDataGrid.Rows.Length, 3);
+                
+                // close
+                fileMenuItemfileMenuItem.Click();
+                System.Threading.Thread.Sleep(500);
+                var exitMenuItemexitMenuItem = fileMenuItemfileMenuItem.FindFirstDescendant(e => e.ByAutomationId("exitMenuItem"));
+                exitMenuItemexitMenuItem.Click();
                 System.Threading.Thread.Sleep(500);
                 var deregelingisgewijzigdOpslaanWindow_1 = window.FindFirstChild(e => e.ByName("De regeling is gewijzigd. Opslaan?"));
                 var noButton7 = deregelingisgewijzigdOpslaanWindow_1.FindFirstDescendant(e => e.ByAutomationId("7"));
