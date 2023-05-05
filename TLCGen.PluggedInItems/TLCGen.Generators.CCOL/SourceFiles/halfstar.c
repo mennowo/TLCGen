992e5d54d50b1dc828a429d4bd43c1895fc2eedb
LexTrafico <d.denouden@ll-t.nl>
2023-05-05 12:58:52 +0200

    Revert "halfstar correcties (halfstar.c -> set_ym_pl_halfstar_fcfc() en halfstar_prio.c -> PrioHalfstarGroenVasthouden() )"
    This reverts commit 70ea2e2a3dbe214de89ffb0198c1b9b8d8fd77a1.

--- TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/halfstar.c
+++ TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/halfstar.c
@@ -477,16 +477,12 @@ void set_ym_pl_halfstar(count fc, bool condition)
 
 void set_ym_pl_halfstar_fcfc(count fc, bool condition, count fc_from, count fc_until)
 {
-#if !defined NO_TIGMAX
-    if (ym_max_tig(fc, NG) &&   /* meeverlengen kan volgens intergroentijden      */
+#if (CCOL_V >= 95)
+    if (ym_max_tig(fc, NG) &&   /* meeverlengen kan volgens ontruimingstijden      */
+        ym_max_trig(fc, NG) &&   /* meeverlengen kan volgens intergroentijdentabel  */
 #else
     if (ym_max_to(fc, NG) &&   /* meeverlengen kan volgens ontruimingstijden      */
-#endif
-
-#if (CCOL_V >= 95)
-        ym_max_trig(fc, NG) &&  /* meeverlengen kan volgens intergroentijdentabel */
-#else
-        ym_max_tig(fc, NG) &&  /* meeverlengen kan volgens intergroentijdentabel  */
+        ym_max_tig(fc, NG) &&   /* meeverlengen kan volgens intergroentijdentabel  */
 #endif
         ym_max_halfstar(fc, 10) &&   /* meeverlengen kan volgens signaalplan            */
         hf_wsg_fcfc(fc_from, fc_until) &&   /* minimaal 1 richting actief                      */
--- TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/halfstar_prio.c
+++ TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/halfstar_prio.c
@@ -718,18 +718,13 @@ void PrioHalfstarGroenVasthouden(void)
 		for (prio = 0;
 			prio < prioFCMAX;
 			prio++) {
-			fc = iFC_PRIOix[prio];
 
+			fc = iFC_PRIOix[prio];
+			magUitstellen = StartGroenConflictenUitstellen(prio, fc, iPrioriteitsOpties[prio]);
+			// Reset PRIO_YV_BIT, will determine YV according to signalplan structure
 			YV[fc] &= ~PRIO_YV_BIT;
 			YM[fc] &= ~PRIO_YM_BIT;
-		}
 
-		for (prio = 0;
-			prio < prioFCMAX;
-			prio++) {
-			fc = iFC_PRIOix[prio];
-
-			magUitstellen = StartGroenConflictenUitstellen(prio, fc, iPrioriteitsOpties[prio]);
 			if (iPrioriteit[prio] &&
 				(iPrioriteitsOpties[prio] & poGroenVastHouden) || (iPrioriteitsOpties[prio] & poPLGroenVastHoudenNaTXD)) {
 
