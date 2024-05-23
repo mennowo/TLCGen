ab0a49cc7a0fe200ae9fc1ef0c0a736658e7924f
Menno van der Woude <mennowo@gmail.com>
2024-05-23 11:35:34 +0200

    Peloton koppelingen hernoemd

--- TLCGen.Dependencies/TLCGen.Dependencies/Models.Enumerations/PelotonKoppelingTypeEnum.cs
+++ TLCGen.Dependencies/TLCGen.Dependencies/Models.Enumerations/PelotonKoppelingTypeEnum.cs
@@ -1,8 +1,14 @@
-﻿namespace TLCGen.Models.Enumerations
+﻿using System.ComponentModel;
+using TLCGen.Helpers;
+
+namespace TLCGen.Models.Enumerations
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum PelotonKoppelingTypeEnum
     {
+        [Description("Type 1")]
         DenHaag,
+        [Description("Type 2")]
         RHDHV
     }
 }
