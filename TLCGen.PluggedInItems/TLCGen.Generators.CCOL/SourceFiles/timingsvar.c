503772c3f847ccfea28b4f65850783b6dd7ada3b
Menno van der Woude <mennowo@gmail.com>
2024-05-24 11:10:38 +0200

    dubbele globale declaratie verholpen

--- TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/timingsvar.c
+++ TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/timingsvar.c
@@ -1,6 +1,6 @@
 ﻿/* Definitie EventState */
 /* ==================== */
-s_int16 CCOL_FC_EVENTSTATE[FCMAX][3];
+extern s_int16 CCOL_FC_EVENTSTATE[FCMAX][3];
 
 s_int16 set_fctiming(mulv i, mulv eventnr, s_int16 mask, s_int16 eventState, s_int16 startTime, s_int16 minEndTime, s_int16 maxEndTime, s_int16 likelyTime, s_int16 confidence, s_int16 nextTime);
 s_int16 reset_fctiming(mulv i, mulv eventnr);
