22bd3f14be84458633631b97bad4c0124950b567
LexTrafico <d.denouden@ll-t.nl>
2024-09-26 23:24:19 +0200

    Specificatie generator

--- TLCGen.PluggedInItems/TLCGen.Specificator/SpecificationGenerator.cs
+++ TLCGen.PluggedInItems/TLCGen.Specificator/SpecificationGenerator.cs
@@ -1,4 +1,5 @@
-﻿using DocumentFormat.OpenXml;
+﻿using System;
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using TLCGen.Generators.CCOL.CodeGeneration;
 using TLCGen.Models;
+using TLCGen.Plugins;
 using Table = DocumentFormat.OpenXml.Wordprocessing.Table;
 
 namespace TLCGen.Specificator
@@ -29,44 +31,6 @@ namespace TLCGen.Specificator
                 return _texts;
             }
         }
-
-        //public static void AddSettingsTable(WordprocessingDocument doc, List<CCOLElement> elements)
-        //{
-        //    Table table = new Table();
-        //
-        //    TableProperties props = new TableProperties(
-        //        new TableBorders(
-        //        new TopBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
-        //        new BottomBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
-        //        new LeftBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
-        //        new RightBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
-        //        new InsideHorizontalBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 },
-        //        new InsideVerticalBorder { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 1 }),
-        //        new TableWidth() { Type = TableWidthUnitValues.Pct, Width = $"{100 * 50}" });
-        //
-        //    table.AppendChild(props);
-        //
-        //    AddRowToTable(
-        //            table,
-        //            new[] { "Type", "Naam", "Instelling", "Commentaar" },
-        //            new[] { 14, 14, 14, 58 });
-        //
-        //    foreach (var e in elements)
-        //    {
-        //        AddRowToTable(
-        //            table,
-        //            new[] { e.Type.ToString(), e.Naam, e.Instelling.ToString(), e.Commentaar },
-        //            new[] { 14, 14, 14, 58 });
-        //    }
-        //
-        //    doc.MainDocumentPart.Document.Body.Append(new Paragraph(new Run(table)));
-        //}
-
-        
-        
-
-        
-
         public static void SetDirtyFlag(WordprocessingDocument doc)
         {
             var settingsPart = doc.MainDocumentPart.GetPartsOfType<DocumentSettingsPart>().First();
@@ -76,15 +40,82 @@ namespace TLCGen.Specificator
             settingsPart.Settings.Save();
         }
 
+        private static void AppendGenericSpecificationData(WordprocessingDocument doc, IEnumerable<SpecificationData> pluginData)
+        {
+            var body = doc.MainDocumentPart.Document.Body;
+
+            foreach (var specData in pluginData.Where(x => (x.Subject == SpecificationSubject.DynHiaat)
+                                                        || (x.Subject == SpecificationSubject.AFM)
+                                                        ))
+            {
+                // verwerken spec data uit plugins
+                foreach (var element in specData.Elements)
+                {
+                    switch (element)
+                    {
+                        case SpecificationParagraph paragraph:
+                            switch (paragraph.Type)
+                            {
+                                case SpecificationParagraphType.Body:
+                                    body.Append(OpenXmlHelper.GetTextParagraph($"{paragraph.Text}"));
+                                    break;
+                                case SpecificationParagraphType.Header1:
+                                    body.Append(OpenXmlHelper.GetTextParagraph($"{paragraph.Text}", "Heading1", true));
+                                    break;
+                                case SpecificationParagraphType.Header2:
+                                    body.Append(OpenXmlHelper.GetTextParagraph($"{paragraph.Text}", "Heading2", true));
+                                    break;
+                                case SpecificationParagraphType.TableHeader:
+                                    TableGenerator.UpdateTables($"{paragraph.Text}");
+                                    body.Append(OpenXmlHelper.GetTextParagraph($"Tabel {TableGenerator.NumberOfTables.ToString()}: {paragraph.Text}", styleid: "Caption"));
+                                    break;
+                                case SpecificationParagraphType.Spacer:
+                                    body.Append(OpenXmlHelper.GetTextParagraph($"{paragraph.Text}", styleid: "Footer"));
+                                    break;
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+                            }
+                            break;
+
+                        case SpecificationTable table:
+                            var docTable = OpenXmlHelper.GetTable(table.TableData, firstRowVerticalText: true);
+                            body.Append(docTable);
+                            break;
+
+                        case SpecificationTable2 table:
+                            var docTable2 = OpenXmlHelper.GetTable(table.TableData2, firstRowVerticalText: false);
+                            body.Append(docTable2);
+                            break;
+
+                        case SpecificationBulletList bulletList:
+                            var docBulletList = OpenXmlHelper.GetBulletList(doc, bulletList.BulletData);
+                            body.Append(docBulletList);
+                            break;
+                    }
+                }
+            }
+        }
+
         public static void GenerateSpecification(string filename, ControllerModel c, SpecificatorDataModel model)
         {
+            var pluginData = new List<SpecificationData>();
+
+            // loopen van alle plugins
+            foreach (var plugin in TLCGenPluginManager.Default.ApplicationPlugins)
+            {
+                // check of de plugin specificatie data heeft
+                if (plugin.Item2 is ITLCGenHasSpecification specGen)
+                {
+                    var d = specGen.GetSpecificationData(c);
+                    if (d != null) pluginData.Add(d);
+                }
+            }
+
             using (var doc = WordprocessingDocument.Open(filename, true))
             {
                 // Add a main document part. 
                 var body = doc.MainDocumentPart.Document.Body;
-                body.RemoveAllChildren<Paragraph>();
-
-                //var gensWithElems = CCOLGenerator.GetAllGeneratorsWithElements(c);
+                //body.RemoveAllChildren<Paragraph>();
 
                 // Headers, title page, versioning
                 FunctionalityGenerator.AddHeaderTextsToDocument(doc, model, c.Data);
@@ -94,20 +125,27 @@ namespace TLCGen.Specificator
                 
                 // Chap 1: Introduction
                 FunctionalityGenerator.GetIntroChapter(doc, c, model);
+                AppendGenericSpecificationData(doc, pluginData.Where(x => x.Subject == SpecificationSubject.Intro));
 
                 // Chap 2: Structuur en afwikkeling
                 body.Append(OpenXmlHelper.GetChapterTitleParagraph($"{Texts["Title_StructuurEnAfwikkeling"]}", 1));
                 body.Append(FunctionalityGenerator.GetChapter_StructureIntroduction(c));
-                body.Append(FunctionalityGenerator.GetChapter_Perioden(c));
                 body.Append(FunctionalityGenerator.GetChapter_SignaalGroepAfhandeling(doc, c));
                 body.Append(FunctionalityGenerator.GetChapter_SignaalGroepInstellingen(doc, c));
+                body.Append(FunctionalityGenerator.GetChapter_Perioden(c));
                 body.Append(FunctionalityGenerator.GetChapter_Groentijden(c));
                 body.Append(FunctionalityGenerator.GetChapter_Modulestructuur(doc, c));
+                body.Append(FunctionalityGenerator.GetChapter_VasteAanvragen(c));
+                body.Append(FunctionalityGenerator.GetChapter_Meeverlengen(c));
+                body.Append(FunctionalityGenerator.GetChapter_Veiligheidsgroen(c));
 
                 // Chap 3: Detectoren
                 body.Append(OpenXmlHelper.GetChapterTitleParagraph($"{Texts["Title_Detectoren"]}", 1));
                 body.Append(FunctionalityGenerator.GetChapter_DetectieConfiguratie(doc, c));
                 body.Append(FunctionalityGenerator.GetChapter_DetectieInstellingen(doc, c));
+
+                AppendGenericSpecificationData(doc, pluginData.Where(x => x.Subject == SpecificationSubject.DynHiaat));
+
                 body.Append(FunctionalityGenerator.GetChapter_DetectieRichtingGevoelig(c));
                 body.Append(FunctionalityGenerator.GetChapter_DetectieStoring(doc, c));
 
@@ -117,6 +155,7 @@ namespace TLCGen.Specificator
                 body.Append(FunctionalityGenerator.GetChapter_OntruimingstijdenGarantie(c));
                 body.Append(FunctionalityGenerator.GetChapter_Synchronisaties(c, doc));
 
+                // Chap 5: Prio ingrepen 
                 if (c.PrioData.PrioIngreepType != Models.Enumerations.PrioIngreepTypeEnum.Geen && c.HasPTorHD())
                 {
                     if (c.HasPT() && c.HasHD())
@@ -136,11 +175,22 @@ namespace TLCGen.Specificator
                     }
                 }
 
+                // Chap 6: Halfstar
                 if (c.HalfstarData.IsHalfstar)
                 {
                     body.Append(OpenXmlHelper.GetChapterTitleParagraph($"{Texts["Title_Halfstar"]}", 1));
-                    body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Hoofdstuk inzake halfstar regelen.", "TODO")));
+                    body.Append(FunctionalityGenerator.GetChapter_HalfstarIntro(c, doc));
                 }
+
+                // Chap 7: Gegenererde specials
+                body.Append(OpenXmlHelper.GetChapterTitleParagraph($"{Texts["Title_TLCGen_Specials"]}", 1));
+                body.Append(OpenXmlHelper.GetTextParagraph("", "Footer"));
+                body.Append(FunctionalityGenerator.GetChapter_PTP(c));
+                body.Append(FunctionalityGenerator.GetChapter_File(c, doc));
+                body.Append(FunctionalityGenerator.GetChapter_VAontr(c, doc));
+                body.Append(FunctionalityGenerator.GetChapter_Signalen(c));
+                body.Append(FunctionalityGenerator.GetChapter_RobuGroVer(c, doc));
+                body.Append(FunctionalityGenerator.GetChapter_PelotonKoppeling(c, doc));
 
                 if (model.SpecialsParagrafen.Any())
                 {
@@ -156,33 +206,35 @@ namespace TLCGen.Specificator
                     }
                 }
 
-                body.Append(OpenXmlHelper.GetChapterTitleParagraph($"TODO", 1));
-                body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Hoofdstuk OV: details toevoegen, zoals: " +
-                    $"lijnnummers, details rond in/uitmelden, inmelden koplus, .", "TODO")));
-                body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Overige punten, zoals: PTP, VA ontruimen, file ingrepen, rateltikkers, " +
-                    $"ingangen, selectieve detectie, uitgestelde vaste aanvragen, hard meeverlengen, veiligheidsgroen, RoBuGrover, special van plugins zoals AFM, etc.", "TODO")));
-                body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Opnemen verwijzingen naar CCOL benaming van instellingen (?).", "TODO")));
-                
-                // TODO: signaalplannen
+                // Chap 8: AFM
+                AppendGenericSpecificationData(doc, pluginData.Where(x => x.Subject == SpecificationSubject.AFM));
 
-                // dan: afhandeling signaalgroepen (nalopen, gelijkstarten, etc)
-                // tabel functies hiernaartoe verplaatsen (?)
+                // Chap 9: TalkingTraffic
+                if (c.RISData.RISToepassen)
+                {
+                    body.Append(OpenXmlHelper.GetChapterTitleParagraph($"{Texts["Title_TalkingTraffic"]}", 1));
+                    body.Append(OpenXmlHelper.GetTextParagraph("", "Footer"));
+                    body.Append(FunctionalityGenerator.GetChapter_TT_Algemeen(c, doc));
+                    body.Append(FunctionalityGenerator.GetChapter_TT_Instellingen(c));
+                    body.Append(FunctionalityGenerator.GetChapter_TT_UC3_Prioriteren(c, doc));
+                    body.Append(FunctionalityGenerator.GetChapter_TT_UC4_Informeren(c, doc));
+                    body.Append(FunctionalityGenerator.GetChapter_TT_UC5_Optimaliseren(c));
+                }
 
-                // dan: OV, HD
-
-                // dan: overig: fixatie -> onderbrengen in H1 (intro) bij algemene instellingen
-
-                //body.Append(FunctionalityGenerator.GetModulenChapter(doc, c));
-
-                doc.Close();
-                //foreach (var g in gensWithElems)
-                //{
-                //    if (g.Item2.Any())
-                //    {
-                //        AddChapterTitle(doc, $"{g.Item1.GetType().Name.ToString()}", 2);
-                //        AddSettingsTable(doc, g.Item2);
-                //    }
-                //}
+                //body.Append(OpenXmlHelper.GetChapterTitleParagraph($"TODO", 1));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Hoofdstuk OV: details toevoegen, zoals: " +
+                //    $"lijnnummers, details rond in/uitmelden, inmelden koplus, .", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"TODO: Overige punten, zoals: " +
+                //    $"ingangen, selectieve detectie, etc.", "TODO")));
+                //
+                //
+                //body.Append((OpenXmlHelper.GetTextParagraph($"", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"-------------------------------------------------- ", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"", "TODO")));
+                //body.Append((OpenXmlHelper.GetTextParagraph($"TODO: errormelding doc.Close() navragen bij Menno." +
+                //"", "TODO")));
             }
         }
     }
