9e4ce0e33cb4f77d63b23418c9672ae3da6441b0
LexTrafico <d.denouden@ll-t.nl>
2025-03-04 17:17:25 +0100

    Mogelijkheid fixatie tegenhouden

--- TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/fixatie.h
+++ TLCGen.PluggedInItems/TLCGen.Generators.CCOL/SourceFiles/fixatie.h
@@ -11,16 +11,17 @@
  *    first, last  = (opeenvolgende) indici van richtingen die gefixeerd
  *                   dienen te worden
  *    bijkomen     = aanduiding of richtingen nog groen mogen worden
+ *    tegenhouden  = aanduiding of fixatie (kort) tegengehouden moet worden
  *    prml         = primair of alternatieve toedeling van betreffende modulereeks
  *    ml           = huidige module in betreffende modulereeks
  *
  * Voorbeeld:
- *    Fixatie(isFix, 0, FCMAX-1, SCH[schbmfix], PRML, ML);
+ *    Fixatie(isFix, 0, FCMAX-1, SCH[schbmfix], IH[hfixatietegenh], PRML, ML);
  *************************************************************************/
 #if defined MLMAX || defined MLAMAX
-void Fixatie(count isFix, count first, count last, bool bijkomen, bool *prml[], count ml);
+void Fixatie(count isFix, count first, count last, bool bijkomen, bool tegenhouden, bool *prml[], count ml);
 #else
-void Fixatie(count isFix, count first, count last, bool bijkomen);
+void Fixatie(count isFix, count first, count last, bool bijkomen, bool tegenhouden);
 #endif
 
 #endif
