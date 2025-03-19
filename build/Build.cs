155ba6a7ed4f40857b20cef657b08a55a2a640bf
Menno van der Woude <mennowo@gmail.com>
2025-03-19 09:51:43 +0100

    Remove cake, add nuke

---
+++ build/Build.cs
@@ -0,0 +1,63 @@
+using System;
+using System.IO;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    public static int Main () => Execute<Build>(x => x.Compile);
+    
+    private static AbsolutePath OutputDirectory => RootDirectory / "output";
+    
+    static readonly string MsBuildPath = @"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe";
+
+    [Solution] readonly Solution Solution;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            OutputDirectory.CreateOrCleanDirectory();
+
+            RootDirectory.GlobDirectories("**/bin", "**/obj").ForEach(x =>
+            {
+                var fs = Directory.EnumerateFiles(x);
+                fs.ForEach(File.Delete);
+            });
+
+            var project = Solution.GetProject("QIC");
+            if (project != null)
+            {
+                var confusedDirectory = project.Directory / "bin" / "x64" / "Release" / "net8.0-windows" / "Confused";
+                confusedDirectory.CreateOrCleanDirectory();
+            }
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            DotNetRestore();
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            MSBuildTasks.MSBuild(_ => _
+                .SetProcessToolPath(MsBuildPath)
+                .SetProjectFile(Solution.GetProject("TLCGen"))
+                .SetConfiguration("Release"));
+        });
+
+}
